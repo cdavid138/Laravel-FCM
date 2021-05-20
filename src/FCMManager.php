186c496250c25912b8e6ed43e8c00366383abd00
David Connolly <cdavid138@users.noreply.github.com>
2021-05-20 09:38:42 +0100

    Update FCMManager.php

--- src/FCMManager.php
+++ src/FCMManager.php
@@ -9,12 +9,12 @@ class FCMManager extends Manager
 {
     public function getDefaultDriver()
     {
-        return $this->container[ 'config' ][ 'fcm.driver' ];
+        return config('fcm.driver');
     }
 
     protected function createHttpDriver()
     {
-        $config = $this->container[ 'config' ]->get('fcm.http', []);
+        $config = config('fcm.http');
 
         return new Client(['timeout' => $config[ 'timeout' ]]);
     }
